2964d06277b55a5b9bad548c3a85eeb6c55baf41
Arturoo0 <arturoportelles@gmail.com>
2020-01-22 22:41:47 -0500

    increased size range

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ import pygame
 import maze
 import color
 import varSizing
+import sys
 
 pygame.init()
 
-GRID_SIZE = 405
+GRID_SIZE = 133
+if GRID_SIZE > 1500: sys.exit();
+
 WINDOW_SIZE = varSizing.adjustSize(GRID_SIZE)
 display = pygame.display.set_mode(WINDOW_SIZE)
+print(WINDOW_SIZE)
 
 pygame.display.set_caption("MazePy")
 
--- varSizing.py
+++ varSizing.py
@@ -2,7 +2,7 @@
 def adjustSize(gridSize):
 
     size = (700, 700)
-    pixelRange = list(range(700, 901))
+    pixelRange = list(range(600, 901))
 
     for res in pixelRange:
         if res % gridSize == 0:
