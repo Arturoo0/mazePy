1f9187fe8639c0cfe43abfc67b2b769e2449d92d
Arturoo0 <arturoportelles@gmail.com>
2020-01-22 22:28:36 -0500

    added adjust size method

---
+++ varSizing.py
@@ -0,0 +1,12 @@
+
+def adjustSize(gridSize):
+
+    size = (700, 700)
+    pixelRange = list(range(700, 901))
+
+    for res in pixelRange:
+        if res % gridSize == 0:
+            size = res
+            break
+
+    return size
